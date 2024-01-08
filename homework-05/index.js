8cab1a0795ebe1259de1ec67d0b0a65066655921
Yevgen Chabanyuk <evgeniush@gmail.com>
2024-01-08 21:51:30 +0200

    Render home folder

--- homework-05/index.js
+++ homework-05/index.js
@@ -1,27 +1,65 @@
 import { createServer } from 'http';
 import path, { dirname } from 'node:path';
-import { readFile } from 'node:fs/promises';
-import { promises as fsp } from 'node:fs';
+import { readdir } from 'node:fs/promises';
 import { fileURLToPath } from 'node:url';
+import { createReadStream } from 'fs';
+
+
+const PORT = process.env.PORT || 8181;
 
 // make __dirname const available
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const PORT = process.env.PORT || 8181;
-
 createServer(async (req, res) => {
-    const files = await fsp.readdir(path.resolve(__dirname));
-    console.log(files);
-    // const url = req.url === '/' ? '/index.html' : req.url;
-    const filePath = path.join('./homework-05', '/index.html');
     try {
-        const data = await readFile(filePath);
-        res.end(data);
-    } catch (err) {
-        res.statusCode = 404;
-        res.end('"File is not found"');
-    }
-}).listen(PORT);
+        const { method, url } = req;
+        const [name, id] = url.substring(1).split('/');
+        console.log(url);
+        if (name.includes('.')) {
+            const rs = createReadStream(path.resolve(__dirname, name));
+            rs.on('error', () => {
+                res.writeHead(500);
+                res.end('No file found');
+            });
+            rs.pipe(res);
+        } else {
+            const folder = await readdir(path.resolve(__dirname, name));
+            if (!folder) {
+                res.writeHead(500);
+                return void res.end('Folder not found');
+            }
+            // const rs = fs.createReadStream(path.resolve(__dirname, filename));
+            console.log(folder);
+            const filteredContents = folder.filter(entity => ![
+                '.DS_Store',
+                'node_modules',
+                'package-lock.json',
+                'favicon.ico',
+                'client.js',
+                'index.js',
+                'package.json',
+            ].includes(entity)).map(ent => `<li><a href="${name}/${ent}">${ent}</a></li>`).join('');
+            const html = `<!DOCTYPE html>
+            <html>
+                <head>
+                    <title>File explorer</title>
+                </head>
+            <body>
+                  <h2>File explorer</h2>
+                  <ul>${filteredContents.toString()}</ul>
+            </body>
+            </html>
+        `;
+            res.setHeader('Content-Type', 'text/html');
+            res.end(filteredContents);
+        }
 
-console.log(`server started at port ${PORT}`);
+    } catch (e) {
+        console.error(e);
+        res.writeHead(500);
+        res.end(`Server crashed`);
+    }
+
+}).listen(PORT);
+console.log(`Server started at port ${PORT}`);
